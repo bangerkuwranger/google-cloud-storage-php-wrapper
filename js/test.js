783eb9c530334a7b065107c79b614ead43004f69
Chad A. Carino <ccarino@sundialbrands.com>
2016-10-13 12:08:45 -0400

    should have finished that js, probably

--- js/test.js
+++ js/test.js
@@ -4,16 +4,16 @@ jQuery(function($) {
 	
 		var action = $('this').val();
 		
-		var aclField = '<label class="col-md-4 control-label" for="aclValue">Acl</label><div class="col-md-4"><input id="aclValue" name="aclValue" type="text" placeholder="entity, [permissions]" class="form-control input-md" ><span class="help-block">Enter the ACL entity, and, if adding or updating, the acl permissions</span>  
-		  </div>
-		</div>
+		var aclField = '<label class="col-md-4 control-label" for="aclValue">Acl</label><div class="col-md-4"><input id="aclValue" name="aclValue" type="text" placeholder="entity, [permissions]" class="form-control input-md" ><span class="help-block">Enter the ACL entity, and, if adding or updating, the acl permissions</span></div></div>';
 		
 		switch (action) {
 		
 			case 2:
 				$('#action-select').append(aclField);
+				break;
 			case 3:
 				$('#action-select').append(aclField);
+				break;
 		
 		}
 	
