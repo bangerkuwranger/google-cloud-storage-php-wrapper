9f5c2099a25a5e28e3cee0fe6be28269fcf98971
Chad A. Carino <ccarino@sundialbrands.com>
2016-10-13 12:09:45 -0400

    yep, definitely should have.

--- js/test.js
+++ js/test.js
@@ -2,7 +2,7 @@ jQuery(function($) {
 
 	$('#action').change( function() {
 	
-		var action = $('this').val();
+		var action = $(this).val();
 		
 		var aclField = '<label class="col-md-4 control-label" for="aclValue">Acl</label><div class="col-md-4"><input id="aclValue" name="aclValue" type="text" placeholder="entity, [permissions]" class="form-control input-md" ><span class="help-block">Enter the ACL entity, and, if adding or updating, the acl permissions</span></div></div>';
 		
