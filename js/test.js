556730c4e0b318181f83d7c70a44c84187dcc224
Chad A. Carino <ccarino@sundialbrands.com>
2016-10-14 12:20:34 -0400

    reorder fields.

--- js/test.js
+++ js/test.js
@@ -15,19 +15,16 @@ jQuery(function($) {
 
 		var aclEntityField = '<div id="aclEntityField" class="form-group"><label class="col-md-4 control-label" for="aclEntityType">ACL Entity Type</label><div class="col-md-4"><select id="aclEntityType" name="aclEntityType" class="form-control"><option value="user-">User</option><option value="group-">group</option><option value="domain-">Domain</option><option value="project-">Project</option><option value="allUsers">All Users</option><option value="allAuthenticatedUsers">All Authenticated Users</option></select></div></div><div class="form-group"><label class="col-md-4 control-label" for="aclEntityValue">Acl Entity</label><div class="col-md-4"><input id="aclEntityValue" name="aclEntityValue" type="text" placeholder="Acl::role" class="form-control input-md" ><span class="help-block">Enter the ACL role</span></div></div></div>';
 		var aclRoleField = '<div id="aclRoleField" class="form-group"><label class="col-md-4 control-label" for="aclRole">ACL Role</label><div class="col-md-4"><select id="aclRole" name="aclRole" class="form-control"><option value="OWNER">Owner</option><option value="READER">Reader</option><option value="WRITER">Writer</option></select></div></div>';
-		
+		clearTestFields();
 		switch (action) {
 		
 			case '2':
-				clearTestFields();
-				$('#action-select').after(aclEntityField);
 				$('#action-select').after(aclRoleField);
+				$('#action-select').after(aclEntityField);
 				break;
 			case '3':
-				clearTestFields();
 				$('#action-select').after(aclEntityField);
 				break;
-			default:
 				
 		
 		}
