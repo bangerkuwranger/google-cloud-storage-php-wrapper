28b9bc5b7eb5ad9409a7be92ecb16ea1528ad753
Chad A. Carino <ccarino@sundialbrands.com>
2016-10-14 12:29:57 -0400

    html inserting is... iffy.

--- js/test.js
+++ js/test.js
@@ -13,7 +13,7 @@ jQuery(function($) {
 	
 	function showTestFields( action ) {
 
-		var aclEntityField = '<div id="aclEntityField" class="form-group"><label class="col-md-4 control-label" for="aclEntityType">ACL Entity Type</label><div class="col-md-4"><select id="aclEntityType" name="aclEntityType" class="form-control"><option value="user-">User</option><option value="group-">group</option><option value="domain-">Domain</option><option value="project-">Project</option><option value="allUsers">All Users</option><option value="allAuthenticatedUsers">All Authenticated Users</option></select></div></div><div class="form-group"><label class="col-md-4 control-label" for="aclEntityValue">Acl Entity</label><div class="col-md-4"><input id="aclEntityValue" name="aclEntityValue" type="text" placeholder="Acl::role" class="form-control input-md" ><span class="help-block">Enter the ACL role</span></div></div></div>';
+		var aclEntityField = '<div id="aclEntityField-type" class="form-group"><label class="col-md-4 control-label" for="aclEntityType">ACL Entity Type</label><div class="col-md-4"><select id="aclEntityType" name="aclEntityType" class="form-control"><option value="user-">User</option><option value="group-">group</option><option value="domain-">Domain</option><option value="project-">Project</option><option value="allUsers">All Users</option><option value="allAuthenticatedUsers">All Authenticated Users</option></select></div></div><div id="aclEntityField-entity class="form-group"><label class="col-md-4 control-label" for="aclEntityValue">Acl Entity</label><div class="col-md-4"><input id="aclEntityValue" name="aclEntityValue" type="text" placeholder="entity id or email" class="form-control input-md" ></div></div></div>';
 		var aclRoleField = '<div id="aclRoleField" class="form-group"><label class="col-md-4 control-label" for="aclRole">ACL Role</label><div class="col-md-4"><select id="aclRole" name="aclRole" class="form-control"><option value="OWNER">Owner</option><option value="READER">Reader</option><option value="WRITER">Writer</option></select></div></div>';
 		clearTestFields();
 		switch (action) {
@@ -33,7 +33,7 @@ jQuery(function($) {
 	
 	function clearTestFields() {
 	
-		var allFieldSelectors = '#aclEntityField, #aclRoleField';
+		var allFieldSelectors = '#aclEntityField-entity, #aclEntityField-type, #aclRoleField';
 		$(allFieldSelectors).remove();
 	
 	}
